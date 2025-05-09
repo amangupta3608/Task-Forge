f3d65d9786a0d7a17a1b2ade9542b5eaea2270d6
Aman Gupta <amangupta0213@gmail.com>
2025-05-09 08:11:34 +0530

    First Commit

---
+++ commentService/commentService/src/test/java/com/TaskForge/commentService/CommentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskForge.commentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
